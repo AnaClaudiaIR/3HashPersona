b4796d448ab4c8d1bdc08179615efc60be074e4d
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-01-20 10:28:36 +0100

    subir2

--- src/MainHashMapPersona.java
+++ src/MainHashMapPersona.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.HashMap;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class MainHashMapPersona {
+    public static void main(String[] args) {
+        HashMap<String, Integer> personas = new HashMap<>();
+        personas.put("Marcelo", 10);
+        personas.put("Laura", 25);
+        personas.put("Estefan", 45);
+        personas.put("Gabriela", 3);
+        personas.put("María", 79);
+
+        //Imprimir los elementos del mapa --> Se obtiene la clave y el valor de esta.
+        for (String key : personas.keySet()) {
+            System.out.println(key + " - " + personas.get(key));
         }
+
+        System.out.println(personas.get("Marcelo")); //Obtener valor según la clave --> Edad
     }
 }
